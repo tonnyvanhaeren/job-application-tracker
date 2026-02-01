b78ea09b3bfaf4ae3038ad37ec0fb39a94d6332d
antonius-vanhaeren <antonius.vanhaeren.av@gmail.com>
2026-02-01 17:48:53 +0100

    first init after hero page

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,5 @@
+export default function SignIn() {
+  return (
+    <div>SignIn page</div>
+  )
+}
---
+++ components/web/navbar.tsx
@@ -0,0 +1,25 @@
+import { Briefcase } from "lucide-react";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+export default function Navbar() {
+
+  return (
+    <nav className="border-b border-gray-200 bg-white">
+      <div className="container mx-auto flex h-16 items-center px-4 justify-between">
+        <Link href="/" className="flex items-center gap-2 text-xl font-semibold text-primary">
+          <Briefcase />
+          Job Tracker
+        </Link>
+        <div className="flex items-center gap-4">
+          <Link href="/sign-in" >
+            <Button variant="ghost" className="text-gray-700 hover:text-black">Log In</Button>
+          </Link>
+          <Link href="/sign-up">
+            <Button className="bg-primary  hover:bg-primary/80">Start for free</Button>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  )
+}
